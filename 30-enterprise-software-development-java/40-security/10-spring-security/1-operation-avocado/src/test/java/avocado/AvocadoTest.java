ae14b5ca7c153bd5f67e0f68bea0cb27362b1e69
Klaas Devits <klaas.devits@gmail.com>
2021-12-06 11:48:49 +0100

    codelab01-operation-avocado done except for extra assignment

--- 30-enterprise-software-development-java/40-security/10-spring-security/1-operation-avocado/src/test/java/avocado/AvocadoTest.java
+++ 30-enterprise-software-development-java/40-security/10-spring-security/1-operation-avocado/src/test/java/avocado/AvocadoTest.java
@@ -12,7 +12,7 @@ class AvocadoTest extends RestAssuredTest {
 
     @Test
     void getDeployedArmyInfo_givenKnownUsernameAndPasswordEncodedAsBasicAuthenticationHeader_thenShouldAllowAccess() {
-        ArmyInfoDto actual = givenRequestForUser("JMILLER", "THANKS")
+        ArmyInfoDto actual = givenRequestForUser("secureUsername", "securePassword")
                 .when()
                 .get(String.format("%s/%s", ArmyResource.ARMY_RESOURCE_PATH, "Belgium"))
                 .then()
@@ -27,7 +27,7 @@ class AvocadoTest extends RestAssuredTest {
 
     @Test
     void getDeployedArmyInfo_givenKnownUsernameAndWrongPasswordEncodedAsBasicAuthenticationHeader_thenShouldNotAllowAccess() {
-        givenRequestForUser("JMILLER", "JBAKER")
+        givenRequestForUser("secureUsername", "wrongPassword")
                 .when()
                 .get(String.format("%s/%s", ArmyResource.ARMY_RESOURCE_PATH, "Belgium"))
                 .then()
@@ -37,7 +37,37 @@ class AvocadoTest extends RestAssuredTest {
 
     @Test
     void getDeployedArmyInfo_givenUnknownUsernameAndPasswordEncodedAsBasicAuthenticationHeader_thenShouldNotAllowAccess() {
-        givenRequestForUser("FONZ", "AYE")
+        givenRequestForUser("unknownUsername", "securePassword")
+                .when()
+                .get(String.format("%s/%s", ArmyResource.ARMY_RESOURCE_PATH, "Belgium"))
+                .then()
+                .assertThat()
+                .statusCode(UNAUTHORIZED.value());
+    }
+
+    @Test
+    void joinArmy_givenKnownUsernameAndPasswordEncodedAsBasicAuthenticationHeader_thenShouldAllowAccess() {
+        givenRequestForUser("secureUsername", "securePassword")
+                .when()
+                .post(String.format("%s", ArmyResource.ARMY_RESOURCE_PATH))
+                .then()
+                .assertThat()
+                .statusCode(OK.value());
+    }
+
+    @Test
+    void joinArmy_givenKnownUsernameAndWrongPasswordEncodedAsBasicAuthenticationHeader_thenShouldNotAllowAccess() {
+        givenRequestForUser("secureUsername", "wrongPassword")
+                .when()
+                .get(String.format("%s/%s", ArmyResource.ARMY_RESOURCE_PATH, "Belgium"))
+                .then()
+                .assertThat()
+                .statusCode(UNAUTHORIZED.value());
+    }
+
+    @Test
+    void joinArmy_givenUnknownUsernameAndPasswordEncodedAsBasicAuthenticationHeader_thenShouldNotAllowAccess() {
+        givenRequestForUser("unknownUsername", "securePassword")
                 .when()
                 .get(String.format("%s/%s", ArmyResource.ARMY_RESOURCE_PATH, "Belgium"))
                 .then()
